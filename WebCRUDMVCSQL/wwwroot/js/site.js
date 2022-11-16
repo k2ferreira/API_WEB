16fa068e290f77d912639924e5366075bd6a9736
k2ferreira <k2ferreira@gmail.com>
2022-11-16 02:04:26 -0300

    Ajuste nos links, estrutura para novas paginas.

--- WebCRUDMVCSQL/wwwroot/js/site.js
+++ WebCRUDMVCSQL/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//$(document).ready(function () {
+
+//    var frm = document.getElementById("form");
+
+//    frm.onsubmit = function (event) {
+//        frm.action = '@Url.Content("~/ProcessarLogin/User")';
+//    }
+//});
